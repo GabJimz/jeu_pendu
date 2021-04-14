3f26244538bd07efbce40e3774b0efdb3108489e
GabJimz <46873801+GabJimz@users.noreply.github.com>
2021-04-14 16:49:25 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const LETTRES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const MOT = "Bonjour";
+
+class App extends Component {
+  render (
+
+  )
+}
+
+export default App();
